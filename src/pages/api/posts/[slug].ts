9fb2a386692bf1ebd175109fdab66cfc86001c0d
Leandro Arturi <lea.arturi@gmail.com>
2025-01-24 15:36:51 -0300

    Initial commit

--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -6,25 +6,60 @@ export const prerender = false
 export const GET: APIRoute = async ({ params }) => {
   const { slug } = params
 
+  let post
   if (slug) {
-    const post = await getEntry('blog', slug)
+    post = await getEntry('blog', slug as any)
 
-    if (post) {
-      return new Response(JSON.stringify(post), {
-        status: 200,
-        headers: {
-          'Content-Type': 'application/json'
+    if (!post) {
+      return new Response(
+        JSON.stringify({
+          error: 'Post not found'
+        }),
+        {
+          status: 404,
+          headers: {
+            'Content-Type': 'application/json'
+          }
         }
-      })
+      )
     }
   }
 
+  return new Response(JSON.stringify(post), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+}
+
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json()
+
   return new Response(
     JSON.stringify({
-      error: 'Post not found'
+      ...body,
+      method: 'POST'
     }),
     {
-      status: 404,
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  )
+}
+
+export const PUT: APIRoute = async ({ request }) => {
+  const body = await request.json()
+
+  return new Response(
+    JSON.stringify({
+      ...body,
+      method: 'PUT'
+    }),
+    {
+      status: 200,
       headers: {
         'Content-Type': 'application/json'
       }

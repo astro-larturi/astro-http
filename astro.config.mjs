60fbca90e4cd6b505230ccc8dc7bdb1bf804daed
Leandro Arturi <lea.arturi@gmail.com>
2025-01-30 14:06:08 -0300

    Vue

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,12 @@ import node from '@astrojs/node'
 
 import db from '@astrojs/db';
 
+import vue from '@astrojs/vue';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), db()],
+  integrations: [mdx(), sitemap(), db(), vue()],
 
   // https://5-0-0-beta.docs.astro.build/en/guides/upgrade-to/v5/#removed-hybrid-rendering-mode
   output: 'static',

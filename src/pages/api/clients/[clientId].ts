fed3d5c1ae5f08727381a74be9c1763a52579101
Leandro Arturi <lea.arturi@gmail.com>
2025-02-27 13:56:37 -0300

    Implement client retrieval, update, and deletion in API; handle not found errors

--- src/pages/api/clients/[clientId].ts
+++ src/pages/api/clients/[clientId].ts
@@ -1,10 +1,38 @@
+import prisma from '@/db'
 import type { APIRoute } from 'astro'
 
 export const prerender = false
 
+const findClientbyId = async (clientId: string) => {
+  try {
+    const client = await prisma.client.findUnique({
+      where: {
+        id: clientId
+      }
+    })
+    return client
+  } catch (error) {
+    console.error(error)
+    return null
+  }
+}
+
 export const GET: APIRoute = async ({ params, request }) => {
   try {
-    return new Response(JSON.stringify({}), {
+    const { clientId = '' } = params
+
+    const client = await findClientbyId(clientId)
+
+    if (!client) {
+      return new Response(
+        JSON.stringify({
+          error: `No se ha encontrado el cliente con el id ${params.clientId}`
+        }),
+        { status: 404 }
+      )
+    }
+
+    return new Response(JSON.stringify(client), {
       status: 200,
       headers: {
         'Content-Type': 'application/json'
@@ -22,8 +50,30 @@ export const GET: APIRoute = async ({ params, request }) => {
 
 export const PATCH: APIRoute = async ({ params, request }) => {
   try {
-    return new Response(JSON.stringify({}), {
-      status: 201,
+    const { clientId = '' } = params
+
+    const client = await findClientbyId(clientId)
+
+    if (!client) {
+      return new Response(
+        JSON.stringify({
+          error: `No se ha encontrado el cliente con el id ${params.clientId}`
+        }),
+        { status: 404 }
+      )
+    }
+
+    const { id, ...body } = await request.json()
+
+    const updatedClient = await prisma.client.update({
+      where: {
+        id: clientId
+      },
+      data: body
+    })
+
+    return new Response(JSON.stringify(updatedClient), {
+      status: 200,
       headers: {
         'Content-Type': 'application/json'
       }
@@ -40,6 +90,25 @@ export const PATCH: APIRoute = async ({ params, request }) => {
 
 export const DELETE: APIRoute = async ({ params, request }) => {
   try {
+    const { clientId = '' } = params
+
+    const client = await findClientbyId(clientId)
+
+    if (!client) {
+      return new Response(
+        JSON.stringify({
+          error: `No se ha encontrado el cliente con el id ${params.clientId}`
+        }),
+        { status: 404 }
+      )
+    }
+
+    await prisma.client.delete({
+      where: {
+        id: clientId
+      }
+    })
+
     return new Response(
       JSON.stringify({
         msg: 'Cliente eliminado correctamente'
